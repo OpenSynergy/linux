741049d8f8328c09dd4daed524e7db690a334919
Peter Hilber <peter.hilber@opensynergy.com>
2023-06-30 11:44:08 +0200

    virtio_rtc: Add PTP clocks
    Expose the virtio_rtc clocks as PTP clocks to userspace, similar to
    ptp_kvm. virtio_rtc can expose multiple clocks, e.g. a UTC clock and a
    monotonic clock. Userspace should distinguish different clocks through the
    name assigned by the driver. A udev rule such as the following can be used
    to get a symlink /dev/ptp_virtio to the UTC clock:
    
    	SUBSYSTEM=="ptp", ATTR{clock_name}=="Virtio PTP UTC", SYMLINK += "ptp_virtio"
    
    The preferred PTP clock reading method is ioctl PTP_SYS_OFFSET_PRECISE2,
    through the ptp_clock_info.getcrosststamp() op. For now,
    PTP_SYS_OFFSET_PRECISE2 will return -EOPNOTSUPP through a weak function.
    PTP_SYS_OFFSET_PRECISE2 requires cross-timestamping support for specific
    clocksources, which will be added in the following. If the clocksource
    specific code is enabled, check that the Virtio RTC device supports the
    respective HW counter before obtaining an actual cross-timestamp from the
    Virtio device.
    
    The Virtio RTC device response time may be higher than the timekeeper
    seqcount increment interval. Therefore, obtain the cross-timestamp before
    calling get_device_system_crosststamp().
    
    As a fallback, support the ioctl PTP_SYS_OFFSET_EXTENDED2 for all
    platforms.
    
    Assume that concurrency issues during PTP clock removal are avoided by the
    posix_clock framework.
    
    Kconfig recursive dependencies prevent virtio_rtc from implicitly enabling
    PTP_1588_CLOCK, therefore just warn the user if PTP_1588_CLOCK is not
    available. Since virtio_rtc should in the future also expose clocks as RTC
    class devices, it should make sense to not have VIRTIO_RTC depend on
    PTP_1588_CLOCK.
    
    Signed-off-by: Peter Hilber <peter.hilber@opensynergy.com>

--- drivers/virtio/virtio_rtc_internal.h
+++ drivers/virtio/virtio_rtc_internal.h
@@ -9,6 +9,7 @@
 #define _VIRTIO_RTC_INTERNAL_H_
 
 #include <linux/types.h>
+#include <linux/ptp_clock_kernel.h>
 
 /* driver core IFs */
 
@@ -19,5 +20,66 @@ int viortc_read_cross(struct viortc_dev *viortc, u64 vio_clk_id, u16 hw_counter,
 		      u64 *reading, u64 *cycles);
 int viortc_cross_cap(struct viortc_dev *viortc, u64 vio_clk_id, u16 hw_counter,
 		     bool *supported);
+
+/* PTP IFs */
+
+struct viortc_ptp_clock;
+
+#if IS_ENABLED(CONFIG_VIRTIO_RTC_PTP)
+
+struct viortc_ptp_clock *viortc_ptp_register(struct viortc_dev *viortc,
+					     struct device *parent_dev,
+					     u64 vio_clk_id,
+					     const char *ptp_clock_name,
+					     bool try_enable_xtstamp);
+int viortc_ptp_unregister(struct viortc_ptp_clock *vio_ptp,
+			  struct device *parent_dev);
+
+#else
+
+static inline struct viortc_ptp_clock *
+viortc_ptp_register(struct viortc_dev *viortc, struct device *parent_dev,
+		    u64 vio_clk_id, const char *ptp_clock_name,
+		    bool try_enable_xtstamp)
+{
+	return NULL;
+}
+
+int viortc_ptp_unregister(struct viortc_ptp_clock *vio_ptp,
+			  struct device *parent_dev)
+{
+	return -ENODEV;
+}
+
+#endif
+
+/* HW counter IFs */
+
+/**
+ * Maximum # of HW counters which the driver can support - can be increased.
+ */
+#define VIORTC_CAP_HW_COUNTERS 4
+
+/**
+ * viortc_hw_get_counters() - get HW counters present
+ * @hw_counters: virtio_rtc HW counters
+ * @num_hw_counters: number of HW counters
+ *
+ * num_hw_counters must not exceed VIORTC_CAP_HW_COUNTERS.
+ *
+ * Context: Process context.
+ * Return: Zero on success, negative error code otherwise.
+ */
+int viortc_hw_get_counters(const u16 **hw_counters, int *num_hw_counters);
+
+/**
+ * viortc_hw_xtstamp_params() - get HW-specific xtstamp params
+ * @hw_counter: virtio_rtc HW counter type
+ * @cs: clocksource corresponding to hw_counter
+ *
+ * Context: Process context.
+ * Return: Zero on success, negative error code otherwise.
+ */
+int viortc_hw_xtstamp_params(u16 *hw_counter, struct clocksource **cs);
 
 #endif /* _VIRTIO_RTC_INTERNAL_H_ */
